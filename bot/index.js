93eb1ac56a4cd0dbcdd60c08fc41630861b5c8ac
makeup4ever <113110246+makeup4ever@users.noreply.github.com>
2020-03-11 14:21:31 +0000

    Make compile.

--- bot/index.js
+++ bot/index.js
@@ -1,3 +1,3 @@
 const log = require('npmlog');
 const Stellar = require('stellar-sdk');
-const { HORIZON_ENDPOINT, SERVER_CONFIG, BOT_CHECK_BALANCE_
+const { HORIZON_ENDPOINT, SERVER_CONFIG, BOT_CHECK_BALANCE_TIMER, TRANSACTION_MAX_TIME } = r
