74b3ef34e2bb2de7401b9d04b6c8d18bd5e4cf1e
makeup4ever <113110246+makeup4ever@users.noreply.github.com>
2020-03-11 21:20:08 +0000

    bogus

--- bot/index.js
+++ bot/index.js
@@ -3,4 +3,4 @@ const Stellar = require('stellar-sdk');
 const { HORIZON_ENDPOINT, SERVER_CONFIG, BOT_CHECK_BALANCE_TIMER, TRANSACTION_MAX_TIME } = require('../config');
 const server = new Stellar.Server(HORIZON_ENDPOINT, SERVER_CONFIG);
 const { sleep } = require('../modules/utils');
-const { showWallets } = require
+const { showWallets } = require('../modules/wallet');
