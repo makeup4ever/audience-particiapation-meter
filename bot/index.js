6a0e94ef2cc950ad1c1a71d49a1854369fffef00
makeup4ever <113110246+makeup4ever@users.noreply.github.com>
2020-03-11 16:20:22 +0000

    add CI badge

--- bot/index.js
+++ bot/index.js
@@ -1,3 +1,4 @@
 const log = require('npmlog');
 const Stellar = require('stellar-sdk');
-const { HORIZON_ENDPOINT, SERVER_CONFIG, BOT_CHECK_BALANCE_TIMER, TRANSACTION_MAX_TIME } = r
+const { HORIZON_ENDPOINT, SERVER_CONFIG, BOT_CHECK_BALANCE_TIMER, TRANSACTION_MAX_TIME } = require('../config');
+const 
