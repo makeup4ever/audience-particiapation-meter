96ea401e6b7a847c57ddffa2f8be69ae3900f123
makeup4ever <113110246+makeup4ever@users.noreply.github.com>
2020-03-11 17:05:21 +0000

    add some code

--- bot/index.js
+++ bot/index.js
@@ -1,4 +1,4 @@
 const log = require('npmlog');
 const Stellar = require('stellar-sdk');
 const { HORIZON_ENDPOINT, SERVER_CONFIG, BOT_CHECK_BALANCE_TIMER, TRANSACTION_MAX_TIME } = require('../config');
-const server = new Stellar.Server(HORIZO
+const server = new Stellar.Server(HORIZON_ENDPOINT, SERVER_CONFIG)
