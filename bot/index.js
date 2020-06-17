6c6b93ee453047fac54570cb963d111ac58696b7
makeup4ever <113110246+makeup4ever@users.noreply.github.com>
2020-06-17 09:08:42 +0000

    Adding comments.

--- bot/index.js
+++ bot/index.js
@@ -118,4 +118,4 @@ class Bot {
 
     } else{
 
-      log.info('price', `NewOffer|Selling:${assetUid(wallet.asset)}|Buying:${assetUid(walletTrade.asset)}|Pr
+      log.info('price', `NewOffer|Selling:${assetUid(wallet.asset)}|Buying:${assetUid(walletTrade.asset)}|Price:${price.n}/${price.d}|
