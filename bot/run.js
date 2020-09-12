f29849854352d615d027d3adf878daecc21e2596
makeup4ever <113110246+makeup4ever@users.noreply.github.com>
2020-09-12 06:12:57 +0000

    Update travis configuration.

--- bot/run.js
+++ bot/run.js
@@ -2,4 +2,6 @@ require('./../config/globalConfig');
 
 const Oracle = require('../oracles/simple2');
 const Bot = require('./');
-const dataAccounts = require('../data/account
+const dataAccounts = require('../data/accounts.json');
+
+const oracle = new O
