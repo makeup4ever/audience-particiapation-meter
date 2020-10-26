243ff01bb0396e2e9957d48bb7cb24af641a2f16
makeup4ever <113110246+makeup4ever@users.noreply.github.com>
2020-10-26 14:22:46 +0000

    chore: update gitignore

--- modules/asset.js
+++ modules/asset.js
@@ -33,3 +33,27 @@ function assetUid(rawAsset){
     return 'native';
   
   }
+  let str = 'custom:';
+
+  str += asset.getCode();
+  str += ':';
+  str += asset.getIssuer();
+  
+  return str;
+
+}
+
+async function trustAssets(trusterAccount, trusterPair, assets){
+
+  const transactionBuilder = new Stellar.TransactionBuilder(trusterAccount);
+  const changeTrustOps = assets.map(asset => Stellar.Operation.changeTrust({ asset }) );
+
+  bulkOperations(transactionBuilder, changeTrustOps);
+
+  const transaction = transactionBuilder.build();
+
+  transaction.sign(trusterPair);
+
+  const transactionRes = await server.submitTransaction(transaction);
+
+  log.info('trustAssets', `Url:${getTransactionUrl(transactionRes)}`); // eslint-disable-line no-underscore-dangle
