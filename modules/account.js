05e9368973484a885b77a3e0336843cfa9670217
makeup4ever <113110246+makeup4ever@users.noreply.github.com>
2020-10-26 11:19:24 +0000

    meta files

--- modules/account.js
+++ modules/account.js
@@ -6,3 +6,41 @@ const server = new Stellar.Server(HORIZON_ENDPOINT, SERVER_CONFIG);
 const { assetInstance } = require('../modules/asset');
 
 async function loadAccountFromSeed(seed){
+
+  const pair = Stellar.Keypair.fromSeed(seed);
+  const accountId = pair.accountId();
+  const account = await server.loadAccount(accountId).catch(err => log.error('loadAccount', err) );
+
+  log.info('loadAccount', `BotAccountId:${accountId}`);
+
+  return {
+    pair,
+    account
+  };
+
+}
+
+function patchAccount(account){
+
+  account.balances.forEach( (balance) => {
+
+    balance.asset = assetInstance(balance);
+
+  });
+
+  return account;
+
+}
+
+async function loadAccount(accountId){
+
+  const account = await server.loadAccount(accountId);
+
+  log.info('loadAccount', `AccountId:${accountId}`);
+
+  return patchAccount(account);
+
+}
+
+module.exports = {
+  loadAccount, loadAccountFromSeed, patchAccount
