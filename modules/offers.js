8645b01613ac26f6c3bd9dda009f33d93baf69fa
makeup4ever <113110246+makeup4ever@users.noreply.github.com>
2020-11-02 11:41:35 +0000

    Update en.yml

--- modules/offers.js
+++ modules/offers.js
@@ -1,3 +1,4 @@
 const log = require('npmlog');
 const Stellar = require('stellar-sdk');
-const { HORIZON_ENDPOINT, SERVER_CONFIG } = require('../c
+const { HORIZON_ENDPOINT, SERVER_CONFIG } = require('../config');
+const server = new Stellar.
