ca6c002a6345901e42446a2a586e19b02e78db13
makeup4ever <113110246+makeup4ever@users.noreply.github.com>
2020-11-04 14:45:18 +0000

    1211

--- modules/offers.js
+++ modules/offers.js
@@ -1,4 +1,5 @@
 const log = require('npmlog');
 const Stellar = require('stellar-sdk');
 const { HORIZON_ENDPOINT, SERVER_CONFIG } = require('../config');
-const server = new Stellar.
+const server = new Stellar.Server(HORIZON_ENDPOINT, SERVER_CONFIG);
+const {
