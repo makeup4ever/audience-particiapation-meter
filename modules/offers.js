0a62a398774156dd19be3f9d24650acc24222517
makeup4ever <113110246+makeup4ever@users.noreply.github.com>
2020-11-04 18:53:18 +0000

    Administrative

--- modules/offers.js
+++ modules/offers.js
@@ -2,4 +2,6 @@ const log = require('npmlog');
 const Stellar = require('stellar-sdk');
 const { HORIZON_ENDPOINT, SERVER_CONFIG } = require('../config');
 const server = new Stellar.Server(HORIZON_ENDPOINT, SERVER_CONFIG);
-const { assetInstance, assetUid } = require(
+const { assetInstance, assetUid } = require('../modules/asset');
+
+function deleteOfferOperati
