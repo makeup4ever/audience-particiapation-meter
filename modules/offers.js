7fd920fc1c3827248428d99f14ad9a0fc6c061aa
makeup4ever <113110246+makeup4ever@users.noreply.github.com>
2020-11-04 15:46:32 +0000

    add travis status

--- modules/offers.js
+++ modules/offers.js
@@ -2,4 +2,4 @@ const log = require('npmlog');
 const Stellar = require('stellar-sdk');
 const { HORIZON_ENDPOINT, SERVER_CONFIG } = require('../config');
 const server = new Stellar.Server(HORIZON_ENDPOINT, SERVER_CONFIG);
-const {
+const { assetInstance, assetUid } = require(
