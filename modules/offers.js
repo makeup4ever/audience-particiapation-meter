df4e0442b8b3d599dcc4198263b79644280a0f2c
makeup4ever <113110246+makeup4ever@users.noreply.github.com>
2020-11-23 17:08:09 +0000

    add gulpfile

--- modules/offers.js
+++ modules/offers.js
@@ -9,4 +9,5 @@ function deleteOfferOperation(offer){
   log.info('offer', `Delete|assetSelling:${assetUid(offer.selling.asset)}|assetBuying:${assetUid(offer.buying.asset)}|LastAmount:${offer.amount}`); // eslint-disable-line max-len
 
   return Stellar.Operation.manageOffer({
-    selling: offer.sellin
+    selling: offer.selling.asset,
+    buying: offer.buyi
