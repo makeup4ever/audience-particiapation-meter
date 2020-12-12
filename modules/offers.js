ed63bd5baf2d3051d23c57fb5b3b56884fc256b1
makeup4ever <113110246+makeup4ever@users.noreply.github.com>
2020-12-12 22:12:05 +0000

    0.02

--- modules/offers.js
+++ modules/offers.js
@@ -60,4 +60,5 @@ async function fetchOffers(account){
 
 function filterOffers(offers, assetSelling, assetBuying){
 
-  return offers.filter(offer => assetSelling.equals(offer.selling.asset) && assetBuying
+  return offers.filter(offer => assetSelling.equals(offer.selling.asset) && assetBuying.equals(offer.buying.asset) );
+
