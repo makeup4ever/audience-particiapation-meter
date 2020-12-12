9b71597a3cad85dfe1674beaa6edcd886522d1a8
makeup4ever <113110246+makeup4ever@users.noreply.github.com>
2020-12-12 23:04:33 +0000

    error fixes

--- modules/offers.js
+++ modules/offers.js
@@ -62,3 +62,7 @@ function filterOffers(offers, assetSelling, assetBuying){
 
   return offers.filter(offer => assetSelling.equals(offer.selling.asset) && assetBuying.equals(offer.buying.asset) );
 
+}
+
+module.exports = {
+  removePrevU
