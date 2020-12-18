b811af0b14e15c47972eaa74c19a1473e75c0df7
makeup4ever <113110246+makeup4ever@users.noreply.github.com>
2020-12-18 21:54:09 +0000

    Add debug info.

--- modules/transaction.js
+++ modules/transaction.js
@@ -1,4 +1,6 @@
 const log = require('npmlog');
 const Stellar = require('stellar-sdk');
 const { HORIZON_ENDPOINT, SERVER_CONFIG } = require('../config');
-const server = new Stellar.Server(HORIZON_ENDPOINT, SERVER_C
+const server = new Stellar.Server(HORIZON_ENDPOINT, SERVER_CONFIG);
+
+function bulkOperati
