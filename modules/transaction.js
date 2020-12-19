784af9efd1b779ff4301f53f93ae389777c53e91
makeup4ever <113110246+makeup4ever@users.noreply.github.com>
2020-12-19 11:21:11 +0000

    chore(package): update sinon to version 5.0.0

--- modules/transaction.js
+++ modules/transaction.js
@@ -3,4 +3,4 @@ const Stellar = require('stellar-sdk');
 const { HORIZON_ENDPOINT, SERVER_CONFIG } = require('../config');
 const server = new Stellar.Server(HORIZON_ENDPOINT, SERVER_CONFIG);
 
-function bulkOperati
+function bulkOperations(transactionBuilder, opera
