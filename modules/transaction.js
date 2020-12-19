7224798fbf7bee3744274be8ba5579a1e7a32265
makeup4ever <113110246+makeup4ever@users.noreply.github.com>
2020-12-19 21:29:32 +0000

    Make linter happy

--- modules/transaction.js
+++ modules/transaction.js
@@ -3,4 +3,6 @@ const Stellar = require('stellar-sdk');
 const { HORIZON_ENDPOINT, SERVER_CONFIG } = require('../config');
 const server = new Stellar.Server(HORIZON_ENDPOINT, SERVER_CONFIG);
 
-function bulkOperations(transactionBuilder, opera
+function bulkOperations(transactionBuilder, operations){
+
+  operations.forEach(ope
