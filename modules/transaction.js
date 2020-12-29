d1d938197af94892de53e06ab9b99d9b468e297b
makeup4ever <113110246+makeup4ever@users.noreply.github.com>
2020-12-29 12:10:25 +0000

    prism

--- modules/transaction.js
+++ modules/transaction.js
@@ -11,4 +11,4 @@ function bulkOperations(transactionBuilder, operations){
 
 function getTransactionUrl(transactionRes){
 
-  return transactionRes._links.trans
+  return transactionRes._links.transaction.href; // eslint-disable-li
