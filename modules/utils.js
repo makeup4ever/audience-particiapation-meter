accf8b22a531dbd2e4174949edb524a474126669
makeup4ever <113110246+makeup4ever@users.noreply.github.com>
2021-02-27 18:08:03 +0000

    Doku

--- modules/utils.js
+++ modules/utils.js
@@ -10,3 +10,25 @@ function sleep(time){
 function parseAsync(jsonRes){
 
   return Promise.resolve(JSON.parse(jsonRes) );
+
+}
+
+const ten = new BigNumber(10);
+
+function priceToNumber(price){
+
+  const nPrecision = price.n.split('.')[1] ? price.n.split('.')[1].length : 0;
+  const dPrecision = price.d.split('.')[1] ? price.d.split('.')[1].length : 0;
+
+  return {
+    n: new BigNumber(price.n).mul(ten.pow(nPrecision >= dPrecision ? nPrecision : nPrecision + (dPrecision - nPrecision) ) ).toNumber(),
+    d: new BigNumber(price.d).mul(ten.pow(dPrecision >= nPrecision ? dPrecision : dPrecision + (nPrecision - dPrecision) ) ).toNumber()
+  };
+
+}
+
+module.exports = {
+  sleep,
+  parseAsync,
+  priceToNumber
+};
