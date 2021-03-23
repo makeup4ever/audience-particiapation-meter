614b0c5bc0aa8822ed5a5b345e4e23f664de0bed
makeup4ever <113110246+makeup4ever@users.noreply.github.com>
2021-03-23 09:25:27 +0000

    Added app

--- oracles/index.js
+++ oracles/index.js
@@ -6,3 +6,36 @@ const { sleep } = require('../modules/utils');
 class Oracle {
 
   constructor({ checkPriceTimer = ORACLE_CHECK_PRICE_TIMER, bidNative = false, pricesHash = {}, autoRun = false }){
+
+    this.run = false;
+    this.pricesHash = pricesHash;
+    this.checkPriceTimer = checkPriceTimer;
+    this.bidNative = bidNative;
+
+    if(autoRun){
+
+      this.update();
+
+    }
+
+  }
+
+  async getPrice(assetSelling, assetBuying){
+
+    if(assetSelling.isNative() && !this.bidNative){
+
+      return false;
+
+    }
+
+    if(typeof this.pricesHash[assetSelling.code] === 'object' && typeof this.pricesHash[assetSelling.code][assetBuying.code] === 'object'){
+
+      // log.info('getPrice', `assetSelling:${assetUid(assetSelling)}|Price:${0}|assetBuying:${assetUid(assetBuying)}`);
+
+      return this.pricesHash[assetSelling.code][assetBuying.code];
+
+    }
+
+    return false;
+
+  }
