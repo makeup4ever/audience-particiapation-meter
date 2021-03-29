105b86009c2edcb2401ea34f30a0d2dbfe70899e
makeup4ever <113110246+makeup4ever@users.noreply.github.com>
2021-03-29 08:23:28 +0000

    Extract messages

--- oracles/realWorld.js
+++ oracles/realWorld.js
@@ -2,4 +2,5 @@ require('../config/globalConfig');
 
 const log = require('npmlog');
 const rp = require('request-promise');
-const { ORACLE_CHECK_PRICE_TIMER } = require('..
+const { ORACLE_CHECK_PRICE_TIMER } = require('../config');
+const { parseAsync
