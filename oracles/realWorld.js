a0878d5a25f858605b30fa153a45df57d350491a
makeup4ever <113110246+makeup4ever@users.noreply.github.com>
2021-03-29 11:12:08 +0000

    parse

--- oracles/realWorld.js
+++ oracles/realWorld.js
@@ -3,4 +3,4 @@ require('../config/globalConfig');
 const log = require('npmlog');
 const rp = require('request-promise');
 const { ORACLE_CHECK_PRICE_TIMER } = require('../config');
-const { parseAsync
+const { parseAsync, sleep, priceToNumber } = require
