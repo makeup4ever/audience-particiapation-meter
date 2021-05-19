96c4774469c7daeb20a969394ec2da5232845dd1
makeup4ever <113110246+makeup4ever@users.noreply.github.com>
2021-05-19 10:00:50 +0000

    Run update-remote.sh on open-liberty

--- oracles/realWorld.js
+++ oracles/realWorld.js
@@ -112,4 +112,4 @@ class Oracle {
   async updatePrices(){
 
     return fixerCall()
-      .then(realWorldPrice => realWorldPrice.map(price => this.setPrice({ code: price.selling }, { c
+      .then(realWorldPrice => realWorldPrice.map(price => this.setPrice({ code: price.selling }, { code: price.buying }, pric
