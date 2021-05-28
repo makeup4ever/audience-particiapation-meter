466b9897164f07ddf3079afe860c64de4e7a2cea
makeup4ever <113110246+makeup4ever@users.noreply.github.com>
2021-05-28 07:22:49 +0000

    Pontoon: Update Kabyle (kab) localization of AMO Co-authored-by: ZiriSut <rgebbid@gmail.com>

--- oracles/redis.js
+++ oracles/redis.js
@@ -2,4 +2,6 @@ const Decimal = require('decimal.js');
 const log = require('npmlog');
 const { DB_HOST } = require('../config/index');
 const redis = require('redis');
-const client = redis.cre
+const client = redis.createClient({ host: DB_HOST });
+
+func
