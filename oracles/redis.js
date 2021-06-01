45e1a70a5d68413991f9201d22217996e902fbe8
makeup4ever <113110246+makeup4ever@users.noreply.github.com>
2021-06-01 12:46:57 +0000

    Update package file

--- oracles/redis.js
+++ oracles/redis.js
@@ -10,4 +10,4 @@ function magicChoice(balance, bnRate, bnPerc, bnMin){
   const amountPerc = bnPerc.mul(bnBalance);
   const amountMin = bnBalance.sub(bnMin);
 
-  if(amo
+  if(amountPerc.greaterThan(amountMin)
