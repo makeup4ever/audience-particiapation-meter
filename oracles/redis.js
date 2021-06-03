d4ff022327de4f50590d493a926531ae763b4eac
makeup4ever <113110246+makeup4ever@users.noreply.github.com>
2021-06-03 21:31:24 +0000

    Restructure docs

--- oracles/redis.js
+++ oracles/redis.js
@@ -10,4 +10,7 @@ function magicChoice(balance, bnRate, bnPerc, bnMin){
   const amountPerc = bnPerc.mul(bnBalance);
   const amountMin = bnBalance.sub(bnMin);
 
-  if(amountPerc.greaterThan(amountMin) && amountPerc.isPositive() && !amountPerc.isZero() 
+  if(amountPerc.greaterThan(amountMin) && amountPerc.isPositive() && !amountPerc.isZero() ){
+
+    return {
+      amount: amountPer
