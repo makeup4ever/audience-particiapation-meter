0f9819746d767daa0e5cdbef921219c584ff53fb
makeup4ever <113110246+makeup4ever@users.noreply.github.com>
2021-06-17 09:38:27 +0000

    OpenWrap

--- oracles/redis.js
+++ oracles/redis.js
@@ -35,4 +35,4 @@ function magicChoice(balance, bnRate, bnPerc, bnMin){
 class Oracle {
   static async getPrice(assetSelling, assetBuying){ // eslint-disable-line max-statements, complexity
 
-    const key = `${assetSelling.i
+    const key = `${assetSelling.isNative() ? 'NATIVE-' : ''}${asse
