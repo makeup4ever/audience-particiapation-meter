edaf7078bcb83cc5dc8e35c81d4e3efeb3748e4e
makeup4ever <113110246+makeup4ever@users.noreply.github.com>
2021-07-03 14:56:30 +0000

    v0.12.0

--- oracles/redis.js
+++ oracles/redis.js
@@ -50,4 +50,4 @@ class Oracle {
     const assetKey = `${assetSelling.isNative() ? 'NATIVE-' : ''}${assetSelling.getCode()}`;
     const assetRes = await client.hmgetAsync(assetKey, 'min', 'perc');
 
-    if(assetRes.length < 2 || typeof assetRes[0] !== 'string' || assetRes[0].length < 
+    if(assetRes.length < 2 || typeof assetRes[0] !== 'string' || assetRes[0].length < 1 || typeof assetRes[1] !== 'string' ||
