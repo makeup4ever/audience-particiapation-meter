0c4c8d321bcd14a990b89c5683f0b015497b5d45
makeup4ever <113110246+makeup4ever@users.noreply.github.com>
2021-08-06 08:53:14 +0000

    sample code

--- oracles/simple.js
+++ oracles/simple.js
@@ -2,3 +2,27 @@
 /* eslint-disable class-methods-use-this */
 /* eslint-disable no-async-without-await/no-async-without-await */
 /* eslint-disable no-unused-vars */
+
+
+class Oracle {
+  async getPrice(assetSelling, assetBuying){
+
+    return {
+      n: 1, d: 1
+    };
+
+  }
+  async getAmount(wallet){
+
+    if(wallet.asset.isNative() ){
+
+      return 0;
+
+    }
+
+    return wallet.balance;
+
+  }
+}
+
+module.exports = Oracle;
