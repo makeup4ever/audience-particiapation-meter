c8fd25b00ef603dc79e50f3570a938755be470dc
makeup4ever <113110246+makeup4ever@users.noreply.github.com>
2022-05-21 12:30:59 +0000

    Incremental.

--- test/spec/filterOffers.spec.js
+++ test/spec/filterOffers.spec.js
@@ -99,3 +99,36 @@ describe('filterOffers', () => {
 
       const assetSelling = new Stellar.Asset('AS1', 'AS1_ISSUER');
       const assetBuying = new Stellar.Asset('AS2', 'AS2_ISSUER');
+
+      const offers = [
+        {
+          _links: {
+            self: { href: 'https://horizon-testnet.stellar.org/offers/1' },
+            offer_maker: { href: 'https://horizon-testnet.stellar.org/accounts/BOT_ACCOUNT_ID' }
+          },
+          id: 1,
+          paging_token: '1',
+          seller: 'BOT_ACCOUNT_ID',
+          selling: {
+            asset_type: 'credit_alphanum4',
+            asset_code: 'AS1',
+            asset_issuer: 'AS1_ISSUER',
+            asset: assetInstance({
+              asset_code: 'AS1', asset_issuer: 'AS1_ISSUER'
+            })
+          },
+          buying: {
+            asset_type: 'credit_alphanum4',
+            asset_code: 'AS2FAKE',
+            asset_issuer: 'AS2_ISSUER',
+            asset: assetInstance({
+              asset_code: 'AS2FAKE', asset_issuer: 'AS2_ISSUER'
+            })
+          },
+          amount: '250.0000000',
+          price_r: {
+            n: 1,
+            d: 1
+          },
+          price: '1.0000000'
+        }
