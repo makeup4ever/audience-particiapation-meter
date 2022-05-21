580dec0450e6d31ce57ce095934a619872f12c16
makeup4ever <113110246+makeup4ever@users.noreply.github.com>
2022-05-21 12:29:52 +0000

    increased timeout

--- test/spec/filterOffers.spec.js
+++ test/spec/filterOffers.spec.js
@@ -67,3 +67,35 @@ describe('filterOffers', () => {
           paging_token: '1',
           seller: 'BOT_ACCOUNT_ID',
           selling: {
+            asset_type: 'credit_alphanum4',
+            asset_code: 'AS1',
+            asset_issuer: 'AS1_ISSUERFAKE',
+            asset: assetInstance({
+              asset_code: 'AS1', asset_issuer: 'AS1_ISSUERFAKE'
+            })
+          },
+          buying: {
+            asset_type: 'credit_alphanum4',
+            asset_code: 'AS2',
+            asset_issuer: 'AS2_ISSUER',
+            asset: assetInstance({
+              asset_code: 'AS2', asset_issuer: 'AS2_ISSUER'
+            })
+          },
+          amount: '250.0000000',
+          price_r: {
+            n: 1,
+            d: 1
+          },
+          price: '1.0000000'
+        }
+      ];
+
+      expect(filterOffers(offers, assetSelling, assetBuying) ).to.deep.equals([]);
+
+    });
+
+    it('Should not match only same selling issuer', () => {
+
+      const assetSelling = new Stellar.Asset('AS1', 'AS1_ISSUER');
+      const assetBuying = new Stellar.Asset('AS2', 'AS2_ISSUER');
