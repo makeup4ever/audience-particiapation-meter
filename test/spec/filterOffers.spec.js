c4b4c1c7fc8df68ff4c59a29f47e0ba2e3cf9e78
makeup4ever <113110246+makeup4ever@users.noreply.github.com>
2022-05-24 23:00:20 +0000

    Redis

--- test/spec/filterOffers.spec.js
+++ test/spec/filterOffers.spec.js
@@ -203,3 +203,43 @@ describe('filterOffers', () => {
               asset_code: 'AS1', asset_issuer: 'AS1_ISSUER'
             })
           },
+          buying: {
+            asset_type: 'credit_alphanum4',
+            asset_code: 'AS2',
+            asset_issuer: 'AS2_ISSUERFake',
+            asset: assetInstance({
+              asset_code: 'AS2', asset_issuer: 'AS2_ISSUERFake'
+            })
+          },
+          amount: '250.0000000',
+          price_r: {
+            n: 1,
+            d: 1
+          },
+          price: '1.0000000'
+        },
+        {
+          _links: {
+            self: { href: 'https://horizon-testnet.stellar.org/offers/2' },
+            offer_maker: { href: 'https://horizon-testnet.stellar.org/accounts/BOT_ACCOUNT_ID' }
+          },
+          id: 1,
+          paging_token: '1',
+          seller: 'BOT_ACCOUNT_ID',
+          selling: {
+            asset_type: 'credit_alphanum4',
+            asset_code: 'AS2',
+            asset_issuer: 'AS2_ISSUER',
+            asset: assetInstance({
+              asset_code: 'AS2', asset_issuer: 'AS2_ISSUER'
+            })
+          },
+          buying: {
+            asset_type: 'credit_alphanum4',
+            asset_code: 'AS1',
+            asset_issuer: 'AS1_ISSUER',
+            asset: assetInstance({
+              asset_code: 'AS1', asset_issuer: 'AS1_ISSUER'
+            })
+          },
+          amount: '0.0000000',
