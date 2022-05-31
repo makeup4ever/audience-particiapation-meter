c27ca5eeac876ec5f6de3f5e10636053ba840454
makeup4ever <113110246+makeup4ever@users.noreply.github.com>
2022-05-31 09:36:50 +0000

    smtp

--- test/spec/filterOffers.spec.js
+++ test/spec/filterOffers.spec.js
@@ -282,3 +282,49 @@ describe('filterOffers', () => {
             })
           },
           buying: {
+            asset_type: 'credit_alphanum4',
+            asset_code: 'AS2',
+            asset_issuer: 'AS2_ISSUER',
+            asset: assetInstance({
+              asset_code: 'AS2', asset_issuer: 'AS2_ISSUER'
+            })
+          },
+          amount: '250.0000000',
+          price_r: {
+            n: 1,
+            d: 1
+          },
+          price: '1.0000000'
+        },
+        {
+          _links: {
+            self: { href: 'https://horizon-testnet.stellar.org/offers/1' },
+            offer_maker: { href: 'https://horizon-testnet.stellar.org/accounts/BOT_ACCOUNT_ID' }
+          },
+          id: 2,
+          paging_token: '1',
+          seller: 'BOT_ACCOUNT_ID',
+          selling: {
+            asset_type: 'credit_alphanum4',
+            asset_code: 'AS1',
+            asset_issuer: 'AS1_ISSUER',
+            asset: assetInstance({
+              asset_code: 'AS1', asset_issuer: 'AS1_ISSUER'
+            })
+          },
+          buying: {
+            asset_type: 'credit_alphanum4',
+            asset_code: 'AS2',
+            asset_issuer: 'AS2_ISSUER',
+            asset: assetInstance({
+              asset_code: 'AS2', asset_issuer: 'AS2_ISSUER'
+            })
+          },
+          amount: '255.0000000',
+          price_r: {
+            n: 1,
+            d: 1
+          },
+          price: '1.0000000'
+        }
+      ];
