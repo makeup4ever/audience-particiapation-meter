26a66b53f4fc06c02d40993820dff03ff66516bf
makeup4ever <113110246+makeup4ever@users.noreply.github.com>
2022-07-04 19:17:34 +0000

    ver 0.3

--- test/spec/wallet.spec.js
+++ test/spec/wallet.spec.js
@@ -1,1 +1,3 @@
-/* eslint max-nested-callbacks:[2, 5], array-callback-return: 0, camelc
+/* eslint max-nested-callbacks:[2, 5], array-callback-return: 0, camelcase: 0 */
+
+const { getUpWallets, sho
