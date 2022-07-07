3a48db3099b9ff4a305a40a635211f952abdd374
makeup4ever <113110246+makeup4ever@users.noreply.github.com>
2022-07-07 07:22:32 +0000

    Updated labels

--- test/spec/wallet.spec.js
+++ test/spec/wallet.spec.js
@@ -1,4 +1,6 @@
 /* eslint max-nested-callbacks:[2, 5], array-callback-return: 0, camelcase: 0 */
 
 const { getUpWallets, showWallets } = require('../../modules/wallet');
-const { assetInstance } = require('..
+const { assetInstance } = require('../../modules/asset');
+
+describe('Wallet
