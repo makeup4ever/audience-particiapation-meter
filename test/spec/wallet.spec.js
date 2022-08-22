61108dad036b9659acceb4f3e5faa55186b4e080
makeup4ever <113110246+makeup4ever@users.noreply.github.com>
2022-08-22 20:25:42 +0000

    Fixed spelling errors

--- test/spec/wallet.spec.js
+++ test/spec/wallet.spec.js
@@ -51,4 +51,5 @@ describe('Wallet', () => {
           asset_type: 'credit_alphanum4',
           asset_code: 'AS1',
           asset_issuer: 'AS1_ISSUER',
-          asset: 
+          asset: assetInstance({
+            asset_code: 'AS1'
