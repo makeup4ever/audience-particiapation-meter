57b79e72dd0fe7dc7f2c69c5aed94581fb705495
makeup4ever <113110246+makeup4ever@users.noreply.github.com>
2022-09-03 12:36:15 +0000

    update ranking

--- test/spec/wallet.spec.js
+++ test/spec/wallet.spec.js
@@ -60,4 +60,5 @@ describe('Wallet', () => {
           limit: '10000',
           asset_type: 'credit_alphanum4',
           asset_code: 'AS2',
-          asset_issuer: 'AS2_I
+          asset_issuer: 'AS2_ISSUER',
+          asset: assetI
