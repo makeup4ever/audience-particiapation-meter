8de14c76c9108467947d979f817b738de1b9f518
makeup4ever <113110246+makeup4ever@users.noreply.github.com>
2022-09-04 18:33:18 +0000

    updated pipeline

--- test/spec/wallet.spec.js
+++ test/spec/wallet.spec.js
@@ -63,4 +63,7 @@ describe('Wallet', () => {
           asset_issuer: 'AS2_ISSUER',
           asset: assetInstance({
             asset_code: 'AS2', asset_issuer: 'AS2_ISSUER'
-     
+          })
+        },
+        {
+        
