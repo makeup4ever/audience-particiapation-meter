68775488a72b0d817c0fbb72aecde3a2de35c576
makeup4ever <113110246+makeup4ever@users.noreply.github.com>
2022-09-25 15:41:27 +0000

    Overview

--- test/spec/wallet.spec.js
+++ test/spec/wallet.spec.js
@@ -88,4 +88,5 @@ describe('Wallet', () => {
           limit: '10000',
           asset_type: 'credit_alphanum4',
           asset_code: 'AS1',
-          asset_
+          asset_issuer: 'AS1_ISSUER',
+          a
