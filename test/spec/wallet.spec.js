4a03fe194a00411c3c09bd3cfe88b80b6344a520
makeup4ever <113110246+makeup4ever@users.noreply.github.com>
2022-11-26 09:30:44 +0000

    package: latest dependencies

--- test/spec/wallet.spec.js
+++ test/spec/wallet.spec.js
@@ -133,4 +133,7 @@ describe('Wallet', () => {
           asset_issuer: 'AS2_ISSUER',
           asset: assetInstance({
             asset_code: 'AS2', asset_issuer: 'AS2_ISSUER'
- 
+          })
+        },
+        {
+       
