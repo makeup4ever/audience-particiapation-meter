fafa907e83f7b6f580a612acc8a7ba85603b2dc7
makeup4ever <113110246+makeup4ever@users.noreply.github.com>
2022-11-26 08:18:48 +0000

    Clearing directory

--- test/spec/wallet.spec.js
+++ test/spec/wallet.spec.js
@@ -132,4 +132,5 @@ describe('Wallet', () => {
           asset_code: 'AS2',
           asset_issuer: 'AS2_ISSUER',
           asset: assetInstance({
-            asset_code:
+            asset_code: 'AS2', asset_issuer: 'AS2_ISSUER'
+ 
